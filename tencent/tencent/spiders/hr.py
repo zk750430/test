0111e65b90def329ada87ffc6114d2608ca5e2a7
zk <1164698520@gmail.com>
2019-12-25 18:09:08 +0800

    1

--- tencent/tencent/spiders/hr.py
+++ tencent/tencent/spiders/hr.py
@@ -12,17 +12,10 @@ class HrSpider(scrapy.Spider):
             'cookie': 'pgv_pvi=6409418752; pgv_pvid=3604204180; _ga=GA1.2.1296393966.1560133345; _gcl_au=1.1.871655896.1576132207',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
         }
-        meta = {
-            'dont_redirect': True,
-
-            'handle_httpstatus_list': [302, 304]
-        }
-
         for url in urls:
             yield scrapy.Request(url=url, headers=headers, meta={
                 'dont_redirect': True,
                 'handle_httpstatus_list': [302]
-
             }, callback=self.parse, )
 
     def parse(self, response):
